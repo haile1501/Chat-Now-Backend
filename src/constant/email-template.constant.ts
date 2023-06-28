f1a422d0fe2285452ee57c48cca8973b26f9453d
OOP-20212-20205181-TranDucDuy <103793658+OOP-20212-20205181-TranDucDuy@users.noreply.github.com>
2023-06-28 15:53:04 +0700

    update forgot-email html

--- src/constant/email-template.constant.ts
+++ src/constant/email-template.constant.ts
@@ -378,7 +378,7 @@ export const verificationPassWordEmail = (email: string, otp: string, password :
           <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
-                <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Activate Your Account</h1>
+                <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Resert Your New Password</h1>
               </td>
             </tr>
           </table>
@@ -407,42 +407,9 @@ export const verificationPassWordEmail = (email: string, otp: string, password :
                 <p style="margin: 0;">YOUR NEW PASSWORD : ${password} </p>
               </td>
             </tr>
-            <tr>
-              <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                <p style="margin: 0;">Tap the button below to confirm your email address. If you didn't create an account with ChatNow, you can safely delete this email.</p>
-              </td>
-            </tr>
             <!-- end copy -->
   
-            <!-- start button -->
-            <tr>
-              <td align="left" bgcolor="#ffffff">
-                <table border="0" cellpadding="0" cellspacing="0" width="100%">
-                  <tr>
-                    <td align="center" bgcolor="#ffffff" style="padding: 12px;">
-                      <table border="0" cellpadding="0" cellspacing="0">
-                        <tr>
-                          <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href=${verificationPageLink} target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Activate</a>
-                          </td>
-                        </tr>
-                      </table>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-            <!-- end button -->
-  
-            <!-- start copy -->
-            <tr>
-              <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
-                <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                <p style="margin: 0;"><a href=${verificationPageLink} target="_blank">link</a></p>
-              </td>
-            </tr>
-            <!-- end copy -->
-  
+           
             <!-- start copy -->
             <tr>
               <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
